c9e0146dac6cc5a12aa532c74fd424ecd03ea96c
student4 <student4@MBP-Dorota.localdomain>
2019-09-20 14:32:30 +0200

    say hello

--- vars/myPipeline.groovy
+++ vars/myPipeline.groovy
@@ -1,31 +1,3 @@
-def myPipeline() {
-    pipeline{
-        agent any
-           options {
-              timestamps()
-              timeout(time: 5, unit: 'MINUTES') }
-        }
-        stages{
-		stage("fetch from repository"){
-		    steps{
-		        git 'https://github.com/osec-jenkins/demoapp.git'
-		    }
-		}
-		stage("fail randomly"){
-		    steps{
-		        sh 'exit `shuf -i 0-1 -n 1`'
-		    }
-		}
-		stage("build project"){
-		    input {
-		        message 'Shall we continue?'
-		    }
-		    steps{
-		        sh "./mvnw package"
-		        junit testResults: '**/surefire-reports/*.xml'
-		        jacoco()
-		    }
-		}
-        }
-    }
+def sayHello(String name = "World") {
+    sh "echo ${name}"
 }
